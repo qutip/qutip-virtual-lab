0aac75f17615c09781eadc69325925b5193d3f9f
trentfridey <tfri.92@gmail.com>
2023-06-01 23:24:30 -0400

    initialize app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+
+import reactLogo from './assets/react.svg';
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div className="card">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
