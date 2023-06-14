3eb96925b088a6a11b46d7abc8e428de52513179
trentfridey <tfri.92@gmail.com>
2023-06-14 11:34:20 -0400

    clean loadWasm.js

--- src/loadWasm.js
+++ src/loadWasm.js
@@ -1,10 +1,7 @@
 import createModule from './pyjs/pyjs_runtime_browser';
 
-console.log(createModule)
-
 const Module = {}
 const makePyJS = async (callback) => {
-    // eslint-disable-next-line no-undef
     const pyjs = await createModule({ print: callback, error: console.error })
     await pyjs.bootstrap_from_empack_packed_environment(
         'empack_env_meta.json',
