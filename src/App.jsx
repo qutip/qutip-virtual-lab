831e8a0a1c652fa3291af070a0580b3687d00ed3
trentfridey <tfri.92@gmail.com>
2023-06-24 10:02:34 -0400

    add simulation demo

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,80 @@ import './App.css';
 
 import React, {
   useEffect,
+  useRef,
   useState,
 } from 'react';
 
 import { hot } from 'react-hot-loader/root';
 
 import Details from './Details';
-import Footer from './Footer';
 import Laboratory from './Laboratory';
 import loadWasm from './loadWasm';
 import Results from './Results';
 
+export const states = {
+  INIT: 'INIT',
+  LOADING: 'LOADING',
+  SIMULATING: 'SIMULATING',
+  SIMULATED: 'SIMULATED'
+}
+
+const demo = () => {
+  let str = "from qutip import *; import numpy as np; import json;"
+  str += "H = sigmax();" 
+  str += "psi0 = basis(2, 0);"
+  str += "tlist = np.linspace(0, 10, 100);"
+  str += "result = mesolve(H, psi0, tlist, [], [sigmax(), sigmay(), sigmaz()]);"
+  str += "print(json.dumps(result.expect[0].tolist()));"
+  str += "print(json.dumps(result.expect[1].tolist()));"
+  str += "print(json.dumps(result.expect[2].tolist()));"
+  return str
+}
+
 function App() {
-  const [qutipVersion, setQutipVersion] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
+  const pyjsRef = useRef(null)
+  const [simulationState, setSimulationState] = useState(states.INIT)
+  const [simulationResults, setSimulationResults] = useState([])
+  const [resultsBuffer, setResultsBuffer] = useState([])
+  
+  const handleSimulate = async () => {
+    try {
+      if (pyjsRef.current === null) {
+        setSimulationState(states.LOADING)
+        pyjsRef.current = await loadWasm(loadResults)
+      }
+      setSimulationState(states.SIMULATING)
+      await pyjsRef.current.exec(demo())
+    } catch (e) {
+      console.error(e)
+    } finally {
+      setSimulationState(states.SIMULATED)
+    }
+  }
+
+  const loadResults = (results) => {
+    console.log(results, typeof results)
+    setResultsBuffer(state => [...state, JSON.parse(results)])
+  }
+
   useEffect(() => {
-    const init = async () => {
-      setIsLoading(true);
-      const pyjs = await loadWasm(setQutipVersion)
-      const main_scope = pyjs.main_scope()
-      const version = await pyjs.exec("import qutip;  print(qutip.__version__);", main_scope)
-      console.log(version)
-      setIsLoading(false);
-    };
-    // init();
-  }, []);
+    if (simulationState === states.SIMULATED) {
+      setSimulationResults(resultsBuffer)
+    }
+  }, [simulationState])
+
   return (
     <>
       <div className="main">
         <div className="panel--lab">
           <Laboratory />
-          <Details />
+          <Details onSimulate={handleSimulate} simulationState={simulationState}/>
         </div>
         <div className="panel--lab">
-        <Results />
+        <Results simulationResults={simulationResults} simulationState={simulationState}/>
         </div>
       </div>
-      <Footer isLoading={isLoading} qutipVersion={qutipVersion}/>
+      {/* <Footer isLoading={isLoading} qutipVersion={qutipVersion}/> */}
     </>
   );
 }
