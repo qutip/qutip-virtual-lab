0711b1da7d88f97e5ae524a1a705bef1e334fa00
trentfridey <tfri.92@gmail.com>
2023-06-27 22:15:59 -0400

    fix styling of BlochSphere

--- src/BlochSphere.jsx
+++ src/BlochSphere.jsx
@@ -1,47 +1,117 @@
-import React, { useRef } from 'react';
+import React, {
+  Suspense,
+  useRef,
+} from 'react';
 
+import PropTypes from 'prop-types';
 import { Vector3 } from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 
+import mathFont
+  from '@compai/font-noto-sans-math/data/typefaces/normal-400.json';
+import { OrbitControls } from '@react-three/drei';
 import {
   Canvas,
   extend,
   useThree,
 } from '@react-three/fiber';
 
-export default function BlochSphere() {
+extend({ TextGeometry });
+
+const width = 100;
+const height = 120;
+const origin = new Vector3(0, 0, 0);
+
+const SPHERE_RADIUS = 1;
+const yAxis = new Vector3(SPHERE_RADIUS, 0, 0);
+const xAxis = new Vector3(0, 0, SPHERE_RADIUS);
+const zAxis = new Vector3(0, SPHERE_RADIUS, 0);
+const negZAxis = new Vector3(0, -SPHERE_RADIUS, 0);
+
+export default function BlochSphere({ blochVector }) {
   const ref = useRef();
-  const vectorDir = new Vector3(2, 0, 0)
-  const vectorOrigin = new Vector3(0,0,0)
-  const vectorLength = 2
+
   return (
-  <div style={{ width: 200, height: 200 }}>
-    <Canvas>
-      <Controls />
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <mesh position={[1.2, 0, 0]} ref={ref}>
-        <sphereGeometry args={[2, 32, 16]} />
-        <arrowHelper args={[vectorDir, vectorOrigin, vectorLength, 0xffffff, 0.2*vectorLength, 0.2*vectorLength]}/>
-        <meshLambertMaterial color={"blue"} transparent={true} opacity={0.5} />
-      </mesh>
-    </Canvas>
-  </div>
-  )
+    <div style={{ width, height}}>
+      <Canvas
+        orthographic
+        camera={{
+          zoom: 35,
+          position: [5, 5, 5],
+          near: 1,
+          far: 100,
+          top: 5,
+          bottom: -5,
+          right: 5,
+          left: -5,
+        }}
+      >
+        <Suspense fallback={null}>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <mesh position={[0, 0, 0]} ref={ref}>
+            <BlochVector x={1} y={0} z={0} />
+            <sphereGeometry args={[SPHERE_RADIUS, 32, 16]} />
+            <ThreeDimAxis />
+            <meshLambertMaterial
+              color={"#aaaaaa"}
+              transparent={true}
+              opacity={0.5}
+            />
+          </mesh>
+
+          <OrbitControls enablePan={false} enableZoom={false} />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
 
-// TODO: add axes for orientation
+BlochSphere.propTypes = {
+  blochVector: PropTypes.arrayOf(PropTypes.shape({
+    x: PropTypes.number,
+    y: PropTypes.number,
+    z: PropTypes.number,
+  }))
+}
 
-extend({ OrbitControls });
+const BlochVector = ({ x, y, z }) => {
+  const vector = new Vector3(x, y, z);
+  return (
+    <arrowHelper
+      args={[vector, origin, vector.length(), 0xff0000, 0.25, 0.25]}
+    />
+  );
+};
 
-const Controls = () => {
-  const { camera, gl } = useThree();
+const ThreeDimAxis = () => {
+  const axisArrowArgs = [origin, 1.2*SPHERE_RADIUS, 0x252525, 0.1 * SPHERE_RADIUS, 0.1 * SPHERE_RADIUS];
+  return (
+    <>
+      <arrowHelper args={[xAxis, ...axisArrowArgs]} />
+      <arrowHelper args={[yAxis, ...axisArrowArgs]} />
+      <arrowHelper args={[zAxis, ...axisArrowArgs]} />
+      <arrowHelper args={[negZAxis, ...axisArrowArgs]} />
+      <AxisLabel label={"|0⟩"} position={zAxis.multiplyScalar(1.5)} />
+      <AxisLabel label={"|1⟩"} position={negZAxis.multiplyScalar(1.5)} />
+      <AxisLabel label={"x"} position={xAxis.multiplyScalar(1.5)} />
+      <AxisLabel label={"y"} position={yAxis.multiplyScalar(1.5)} />
+    </>
+  );
+};
+
+const AxisLabel = ({ label, position }) => {
+  const axisRef = useRef();
+  const { camera } = useThree();
+  const mathRegular = new FontLoader().parse(mathFont);
 
   return (
-    <orbitControls
-      enableZoom={false}
-      enablePan={false}
-      args={[camera, gl.domElement]}
-    />
+    <mesh position={position} ref={axisRef} quaternion={camera.quaternion}>
+      <textGeometry
+        args={[label, { font: mathRegular, size: 0.30, height: 0.01 }]}
+      />
+      <meshStandardMaterial attach="material" color={"black"} />
+    </mesh>
   );
 };
