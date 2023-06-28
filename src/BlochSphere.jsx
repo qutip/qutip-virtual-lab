e3c63e18f33b029b522aed2a3bc3dbd2d122f91b
trentfridey <tfri.92@gmail.com>
2023-06-28 10:36:54 -0400

    refactor, fix orientations in BlochSphere

--- src/BlochSphere.jsx
+++ src/BlochSphere.jsx
@@ -1,9 +1,10 @@
 import React, {
+  forwardRef,
   Suspense,
+  useMemo,
   useRef,
 } from 'react';
 
-import PropTypes from 'prop-types';
 import { Vector3 } from 'three';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
@@ -24,16 +25,14 @@ const height = 120;
 const origin = new Vector3(0, 0, 0);
 
 const SPHERE_RADIUS = 1;
-const yAxis = new Vector3(SPHERE_RADIUS, 0, 0);
-const xAxis = new Vector3(0, 0, SPHERE_RADIUS);
-const zAxis = new Vector3(0, SPHERE_RADIUS, 0);
-const negZAxis = new Vector3(0, -SPHERE_RADIUS, 0);
+const xAxis = new Vector3(SPHERE_RADIUS, 0, 0);
+const yAxis = new Vector3(0, SPHERE_RADIUS, 0);
+const zAxis = new Vector3(0, 0, SPHERE_RADIUS);
+const negZAxis = new Vector3(0, 0, -SPHERE_RADIUS);
 
-export default function BlochSphere({ blochVector }) {
-  const ref = useRef();
-
+export default function ({ data = [[],[],[]], time }) {
   return (
-    <div style={{ width, height}}>
+    <div style={{ width, height }}>
       <Canvas
         orthographic
         camera={{
@@ -45,48 +44,62 @@ export default function BlochSphere({ blochVector }) {
           bottom: -5,
           right: 5,
           left: -5,
+          up: [0,0,1]
         }}
       >
         <Suspense fallback={null}>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <mesh position={[0, 0, 0]} ref={ref}>
-            <BlochVector x={1} y={0} z={0} />
-            <sphereGeometry args={[SPHERE_RADIUS, 32, 16]} />
-            <ThreeDimAxis />
-            <meshLambertMaterial
-              color={"#aaaaaa"}
-              transparent={true}
-              opacity={0.5}
-            />
-          </mesh>
-
-          <OrbitControls enablePan={false} enableZoom={false} />
+          <BlochSphere data={data} time={time}/>
         </Suspense>
       </Canvas>
     </div>
   );
 }
 
-BlochSphere.propTypes = {
-  blochVector: PropTypes.arrayOf(PropTypes.shape({
-    x: PropTypes.number,
-    y: PropTypes.number,
-    z: PropTypes.number,
-  }))
+function BlochSphere({ data, time }) {
+  const ref = useRef();
+  const blochVectorRef = useRef();
+  const vector = useMemo(() => {
+    const [x,y,z] = data?.map(axis => axis.at(time)) ?? [0,0,0]
+    return {x,y,z}
+  })
+  return (
+    <>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <mesh position={[0, 0, 0]} ref={ref}>
+        <BlochVector x={vector.x} y={vector.y} z={vector.z} ref={blochVectorRef} />
+        <sphereGeometry args={[SPHERE_RADIUS, 32, 16]} />
+        <ThreeDimAxis />
+        <meshLambertMaterial
+          color={"#aaaaaa"}
+          transparent={true}
+          opacity={0.5}
+        />
+      </mesh>
+
+      <OrbitControls enablePan={false} enableZoom={false} />
+    </>
+  );
 }
 
-const BlochVector = ({ x, y, z }) => {
+const BlochVector = forwardRef(({ x, y, z }, ref) => {
   const vector = new Vector3(x, y, z);
-  return (
+  return vector.length() ? (
     <arrowHelper
+      ref={ref}
       args={[vector, origin, vector.length(), 0xff0000, 0.25, 0.25]}
     />
-  );
-};
+  ) : [];
+});
 
 const ThreeDimAxis = () => {
-  const axisArrowArgs = [origin, 1.2*SPHERE_RADIUS, 0x252525, 0.1 * SPHERE_RADIUS, 0.1 * SPHERE_RADIUS];
+  const axisArrowArgs = [
+    origin,
+    1.2 * SPHERE_RADIUS,
+    0x252525,
+    0.1 * SPHERE_RADIUS,
+    0.1 * SPHERE_RADIUS,
+  ];
   return (
     <>
       <arrowHelper args={[xAxis, ...axisArrowArgs]} />
@@ -109,7 +122,7 @@ const AxisLabel = ({ label, position }) => {
   return (
     <mesh position={position} ref={axisRef} quaternion={camera.quaternion}>
       <textGeometry
-        args={[label, { font: mathRegular, size: 0.30, height: 0.01 }]}
+        args={[label, { font: mathRegular, size: 0.3, height: 0.01 }]}
       />
       <meshStandardMaterial attach="material" color={"black"} />
     </mesh>
