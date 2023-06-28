d7ce7cc637ee1a98df7c384b5d567038cdc1aca9
trentfridey <tfri.92@gmail.com>
2023-06-27 22:18:36 -0400

    add LineGraph to Results

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,12 +1,21 @@
+import './Results.css';
+
 import React from 'react';
 
 import BlochSphere from './BlochSphere';
+import LineGraph from './LineGraph';
 
 export default function Results() {
   return (
     <div>
       <h2>Results</h2>
-      <BlochSphere />
+      <div className="result-container">
+        <BlochSphere />
+        <LineGraph />
+      </div>
+      <div className="result-controls">
+        <input type="range"/>
+      </div>
     </div>
   );
 }
