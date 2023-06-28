5d38f55d119204f23c918d32d11aaf53117b0d45
trentfridey <tfri.92@gmail.com>
2023-06-27 22:21:40 -0400

    update port in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    server: {
+        port: 8080
+    }
 });
