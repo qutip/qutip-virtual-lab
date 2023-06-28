3e5745f7ab846f06c43066c4270ed2619129a765
trentfridey <tfri.92@gmail.com>
2023-06-27 23:02:48 -0400

    add hook to Results

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,20 +1,32 @@
 import './Results.css';
 
-import React from 'react';
+import React, {
+  useContext,
+  useState,
+} from 'react';
 
 import BlochSphere from './BlochSphere';
 import LineGraph from './LineGraph';
+import { SimulationContext } from './Simulation';
 
 export default function Results() {
+
+  const {
+    results, 
+} = useContext(SimulationContext)
+
+
+  const [time, setTime] = useState(0)
+
   return (
     <div>
       <h2>Results</h2>
       <div className="result-container">
-        <BlochSphere />
-        <LineGraph />
+        <BlochSphere time={time} data={results}/>
+        <LineGraph time={time} data={results}/>
       </div>
       <div className="result-controls">
-        <input type="range"/>
+        <input type="range" value={time} onChange={setTime}/>
       </div>
     </div>
   );
