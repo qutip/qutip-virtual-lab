3517294b96867acd0ad6cbc0d369932ea20e1baf
trentfridey <tfri.92@gmail.com>
2023-06-28 10:37:10 -0400

    add tabs to Details

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,7 +1,10 @@
 import './Details.css';
 import 'katex/dist/katex.min.css';
 
-import React, { useContext } from 'react';
+import React, {
+  useContext,
+  useState,
+} from 'react';
 
 import { BlockMath } from 'react-katex';
 
@@ -14,16 +17,53 @@ export default function Details() {
 
     const {
         state, 
-        submit
+        reset,
+        submit, setConfig
     } = useContext(SimulationContext)
+
+    const tabs = {
+      LARMOR: 'LARMOR',
+      DEPHASING: 'DEPHASING'
+    }
+
+    const [activeTab, setActiveTab] = useState(tabs.LARMOR)
+    const handleTabClick = (tab) => {
+      setActiveTab(tab)
+      reset()
+      setConfig(() => ({ name: tab}))
+    }
 
   return (
     <div id="details">
       <h2>Details</h2>
-      <div>
-        <BlockMath>
-            {'\\mathcal{H} = \\lambda_1 \\sigma_x^{(1)}'}
-        </BlockMath>
+      <ul className="details-tabs">
+        <li onClick={() => handleTabClick(tabs.LARMOR)} className={`details-tab ${activeTab === tabs.LARMOR ? 'active' : ''}`}>Larmor Precession</li>
+        <li onClick={() => handleTabClick(tabs.DEPHASING)} className={`details-tab ${activeTab === tabs.DEPHASING ? 'active' : ''}`}>Qubit Dephasing</li>
+      </ul>
+      <div className='details-tab--content'>
+        {activeTab === tabs.LARMOR && (
+          <>
+            <BlockMath>
+              {'\\mathcal{H} = \\lambda_1 \\sigma_x^{(1)}'}
+            </BlockMath>
+            <BlockMath>
+              {'|\\psi(0)\\rangle = |0\\rangle'}
+            </BlockMath>
+            </>
+          )}
+          {activeTab === tabs.DEPHASING && (
+            <>
+            <BlockMath>
+              {'\\mathcal{H} = \\Delta(\\cos(\\theta) \\sigma_z^{(1)} + \\sin(\\theta)\\sigma_x^{(1)})'}
+            </BlockMath>
+            <BlockMath>
+              {'C = \\sqrt{\\gamma_p}\\,\\sigma_z^{(1)}'}
+            </BlockMath>
+            <BlockMath>
+              {'|\\psi(0)\\rangle = |0\\rangle'}
+            </BlockMath>
+            </>
+          )}
       </div>
       <button onClick={submit} disabled={[states.SIMULATING, states.LOADING].includes(state)}>
         {state === states.INIT && 'Simulate ☞'}
