a18f18c80b73f5abd9b3cd8eaf591c3dc92d00fe
trentfridey <tfri.92@gmail.com>
2023-07-30 00:23:49 -0400

    add more types; initialStates

--- src/simulationUtils.tsx
+++ src/simulationUtils.tsx
@@ -1,8 +1,12 @@
 export type QubitId = number;
-export type Axis = "x" | "y" | "z";
+export const X = 'x' as const
+export const Y = 'y' as const
+export const Z = 'z' as const
+
+export type Axis = typeof X | typeof Y | typeof Z;
 export type PauliOperatorKey = `S${Axis}`
 
-export const PauliX = { 
+export const PauliX = {
     src: "sigmax()",
     label: "\\sigma_x",
 } as const
@@ -23,6 +27,10 @@ export const PauliOperators: Record<PauliOperatorKey, PauliOperator> = {
     Sy: PauliY,
     Sz: PauliZ
 }
+
+export type InitialState = `${Axis}` | `-${Axis}`
+export const eigenStates: Array<InitialState> = [X, Y, Z, `-${X}`, `-${Y}`, `-${Z}`]
+export const isInitialState = (state): state is InitialState => eigenStates.includes(state)
 
 type SimulationParameter = {
     label: string;
@@ -60,23 +68,27 @@ export interface SimulationConfig {
     lasers: Array<SingleQubitOperator>;
     interactions: Array<InteractionOperator>;
     baths: Array<CollapseOperator>;
+    initialStates: {
+        [key: QubitId]: InitialState
+    };
 }
 
 export const emptyConfig: SimulationConfig = {
     lasers: [],
     interactions: [],
     baths: [],
-    qubits: 0
+    qubits: 0,
+    initialStates: {}
 }
 
 
 export const getDetails = (config: SimulationConfig): SimulationConfigDetails => {
-    const { lasers, interactions, baths, qubits } = config
+    const { lasers, interactions, baths, initialStates } = config
     let interactionTerms: Array<string> = []
     let singleQubitTerms: Array<string> = []
     lasers.forEach(laser => {
-            const str = `${laser.parameter.label} ${laser.operator.label}^{(${laser.qubitId})}`
-            singleQubitTerms = [...singleQubitTerms, str]
+        const str = `${laser.parameter.label} ${laser.operator.label}^{(${laser.qubitId})}`
+        singleQubitTerms = [...singleQubitTerms, str]
     })
     interactions.forEach(interaction => {
         const str = `${interaction.parameter.label} ${interaction.operator.label}^{(${interaction.qubitIds[0]})}`
@@ -91,11 +103,19 @@ export const getDetails = (config: SimulationConfig): SimulationConfigDetails =>
         ...interactions.map(getParameter),
         ...baths.map(getParameter)
     ]
+    const initialState = Object.values(initialStates).map((state) => {
+        if (state === '-z') return '|0\\rangle'
+        if (state === 'z') return '|1\\rangle'
+        if (state === 'x') return '\\frac{1}{\\sqrt{2}}(|0\\rangle + |1\\rangle)'
+        if (state === '-x') return '\\frac{1}{\\sqrt{2}}(|0\\rangle - |1\\rangle)'
+        if (state === 'y') return '\\frac{1}{\\sqrt{2}}(|0\\rangle + i|1\\rangle)'
+        if (state === '-y') return '\\frac{1}{\\sqrt{2}}(|0\\rangle - i|1\\rangle)'
+    }).join('\\otimes')
     return {
         Hamiltonian: H.join('+'),
         parameters,
         collapseOperators: baths,
-        initialState: `|${"0".repeat(qubits)}\\rangle`, // TODO: allow user to set
+        initialState
     };
 }
 
@@ -103,16 +123,16 @@ export const getSrc = (config: SimulationConfig): string => {
     const { lasers, interactions, baths, qubits } = config;
     let imports = "from qutip import *; import numpy as np; import json";
     let tlist = "tlist = np.linspace(0, 10, 100)";
-    let psi0 = `psi0 = basis(${qubits}, 0)`;
+    let psi0 = `psi0 = basis(${qubits}, 0)`; // TODO: fix me
     let H: Array<string> = [];
     let params: Array<string> = [];
     lasers.forEach((laser) => {
-            const { qubitId, operator, parameter } = laser ;
-            const embedded = Array.from({ length: qubits }, (_, i) =>
+        const { qubitId, operator, parameter } = laser;
+        const embedded = Array.from({ length: qubits }, (_, i) =>
             i === qubitId ? `${parameter.src}*${operator.src}` : "qeye(2)"
-            ).join();
-            params = [...params, `${parameter.src} = ${parameter.value}`]
-            H = [...H, `tensor(${embedded})`];
+        ).join();
+        params = [...params, `${parameter.src} = ${parameter.value}`]
+        H = [...H, `tensor(${embedded})`];
     });
     interactions.forEach((interaction) => {
         const { qubitIds, operator, parameter } = interaction;
