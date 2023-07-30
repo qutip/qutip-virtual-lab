cf3bc4a9dc82e164af6083c24cbf94d2f68a03b1
trentfridey <tfri.92@gmail.com>
2023-07-30 00:25:52 -0400

    add ability to select initial state; render collapse operators

--- src/Details.tsx
+++ src/Details.tsx
@@ -4,16 +4,28 @@ import 'katex/dist/katex.min.css';
 import React, { useContext } from 'react';
 
 // @ts-ignore
-import { BlockMath } from 'react-katex';
+import {
+  BlockMath,
+  InlineMath,
+} from 'react-katex';
 
 import { SimulationContext } from './Simulation';
+import { InitialState } from './simulationUtils';
 
 export default function Details() {
-  const { details } =
+  const { details, setConfig, config } =
     useContext(SimulationContext);
-
-  const { Hamiltonian, initialState, collapseOperators, parameters } = details
     
+    const initialStateOptions: Array<InitialState> = ["x", "y", "z", "-x", "-y", "-z"]
+    
+    const { Hamiltonian, initialState, collapseOperators, parameters } = details
+    const { initialStates } = config
+
+  const handleChangeInitialState = (val: InitialState, qubit) => {
+    let newInitialStates = config.initialStates
+    newInitialStates[qubit] = val
+    setConfig(config => ({...config, initialStates: newInitialStates}))
+  }
 
   return (
     <div id="details">
@@ -26,7 +38,18 @@ export default function Details() {
             </div>
             <div className="details-field">
               <label>Initial State</label>
-              <BlockMath>{`|\\psi(0)\\rangle = ${initialState}`}</BlockMath>
+              {Object.entries(initialStates).map(([key, initialState]) => (
+                <div>
+                  <InlineMath>{`|q_${key}\\rangle = |`}</InlineMath>
+                  <select value={initialState} onChange={e => handleChangeInitialState(e.target.value as InitialState, key)}>
+                    {initialStateOptions.map(option => (
+                      <option value={option}>{option}</option>
+                    ))}
+                  </select>
+                  <InlineMath>{'\\rangle'}</InlineMath>
+                </div>
+              ))}
+              <BlockMath>{`|\\psi\\rangle = ${initialState}`}</BlockMath>
             </div>
           </div>
           <div className="details-field-cols">
@@ -34,15 +57,14 @@ export default function Details() {
               <div className="details-field">
                 <label>Collapse Operators</label>
                 {collapseOperators.map((C, i) => (
-                  <BlockMath>{`C_{${i}} = ${C}`}</BlockMath>
+                  <BlockMath>{`C_{${i}} = ${C.parameter.label}${C.operator.label}`}</BlockMath>
                 ))}
               </div>
             )}
             {!!parameters?.length && (
               <div className="details-field">
                 <label>Parameters</label>
-                {parameters.map(({label, value}, i) => 
-                  {return (label) && (<BlockMath>{`${label} = ${value}`}</BlockMath>)}
+                {parameters.map(({ label, value }, i) => { return (label) && (<BlockMath>{`${label} = ${value}`}</BlockMath>) }
                 )}
               </div>
             )}
