edd6abe6820e57c5cdca00da3ffc351b230d0914
trentfridey <tfri.92@gmail.com>
2023-07-30 21:29:49 -0400

    path must be absolute

--- copy_runtime.sh
+++ copy_runtime.sh
@@ -9,10 +9,10 @@ then
 fi
 sudo docker run \
 --user=root \
---mount type=bind,source=./public,target=/runtime qutip-virtual-lab \
+--mount type=bind,source=${PWD}/public,target=/runtime qutip-virtual-lab \
 sh -c "cp -a /tmp/build/pyjs_runtime_browser.wasm /runtime && cp -a /tmp/build/empack/. /runtime/empack/" \
 &&
 sudo docker run \
 --user=root \
---mount type=bind,source=./src/pyjs,target=/runtime qutip-virtual-lab \
+--mount type=bind,source=${PWD}/src/pyjs,target=/runtime qutip-virtual-lab \
 sh -c "cp -a /tmp/build/pyjs_runtime_browser.js /runtime"
