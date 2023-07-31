177a22b51ebcedfed69054cb4358eac092a03fc3
trentfridey <tfri.92@gmail.com>
2023-07-30 20:36:52 -0400

    check for directories before copy

--- copy_runtime.sh
+++ copy_runtime.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+if [ ! -d "./src/pyjs" ]
+then
+    mkdir -p ./src/pyjs
+fi
+if [ ! -d "./public" ]
+then
+    mkdir -p ./public
+fi
 sudo docker run \
 --user=root \
---mount type=bind,source=${PWD}/public,target=/runtime qutip-virtual-lab \
+--mount type=bind,source=./public,target=/runtime qutip-virtual-lab \
 sh -c "cp -a /tmp/build/pyjs_runtime_browser.wasm /runtime && cp -a /tmp/build/empack/. /runtime/empack/" \
 &&
 sudo docker run \
 --user=root \
---mount type=bind,source=${PWD}/src/pyjs,target=/runtime qutip-virtual-lab \
+--mount type=bind,source=./src/pyjs,target=/runtime qutip-virtual-lab \
 sh -c "cp -a /tmp/build/pyjs_runtime_browser.js /runtime"
