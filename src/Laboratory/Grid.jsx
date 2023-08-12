e74790ef4a8282a45780e98fdf5808eb9b3883ec
trentfridey <tfri.92@gmail.com>
2023-08-12 16:09:15 -0400

    fix grid spacing

--- src/Laboratory/Grid.jsx
+++ src/Laboratory/Grid.jsx
@@ -1,96 +1,15 @@
 import {
   Group,
   Line,
-  Rect,
 } from 'react-konva';
 
-const BoundsMarkers = ({ width, height }) => {
-  const l = 30;
-  const padding = l / 2;
-  const numTicks = 4;
-
-  const Reticule = ({ x, y }) => (
-    <>
-      <Rect
-        width={0}
-        height={l}
-        stroke="#aaaaaa33"
-        strokeWidth={3}
-        x={x}
-        y={y - l / 2}
-      />
-      <Rect
-        width={l}
-        height={0}
-        stroke="#aaaaaa33"
-        strokeWidth={3}
-        x={x - l / 2}
-        y={y}
-      />
-    </>
-  );
-
-  return (
-    <>
-      {Array.from({ length: numTicks }).map((_, i) => {
-        const spacing = (width - 2 * l - 4 * padding) / (numTicks - 1);
-        const startOffset = l + 2 * padding;
-        return (
-          <Group key={'b'+i}>
-            <Rect
-              width={2}
-              height={l / 4}
-              fill="orange"
-              stroke="orange"
-              strokeWidth={1}
-              x={startOffset + i * spacing}
-              y={padding}
-            />
-            <Rect
-              width={l / 4}
-              height={2}
-              fill="orange"
-              stroke="orange"
-              strokeWidth={1}
-              y={startOffset + i * spacing}
-              x={padding}
-            />
-            <Rect
-              width={2}
-              height={l / 4}
-              fill="orange"
-              stroke="orange"
-              strokeWidth={1}
-              x={startOffset + i * spacing}
-              y={height - padding - l / 4}
-            />
-            <Rect
-              width={l / 4}
-              height={2}
-              fill="orange"
-              stroke="orange"
-              strokeWidth={1}
-              y={startOffset + i * spacing}
-              x={width - padding - l / 4}
-            />
-          </Group>
-        );
-      })}
-      {/* <Reticule x={padding} y={padding} />
-        <Reticule x={padding} y={height - padding} />
-        <Reticule x={width - padding} y={padding} />
-        <Reticule x={width - padding} y={height - padding} /> */}
-    </>
-  );
-};
-
-const Grid = ({ numSpaces, minor = false, width, height }) => {
-  const numLines = minor ? numSpaces : numSpaces - 1;
-  const spacing = width / numSpaces;
+const Grid = ({ spacing, minor = false, width, height }) => {
+  const numVerticalLines = width / spacing
+  const numHorizontalLines = height / spacing
   const dashProps = minor ? {} : { dash: [2, 10] };
   return (
     <>
-      {Array.from({ length: numLines }).map((_, i) => {
+      {Array.from({ length: numVerticalLines }).map((_, i) => {
         const start = minor
           ? (i + 1) * spacing - spacing / 2
           : (i + 1) * spacing;
@@ -104,6 +23,15 @@ const Grid = ({ numSpaces, minor = false, width, height }) => {
               points={[0, 0, 0, height]}
               {...dashProps}
             />
+          </Group>
+        );
+      })}
+      {Array.from({ length: numHorizontalLines }).map((_, i) => {
+        const start = minor
+          ? (i + 1) * spacing - spacing / 2
+          : (i + 1) * spacing;
+        return (
+          <Group key={'g'+i}>
             <Line
               x={0}
               y={start}
@@ -121,8 +49,7 @@ const Grid = ({ numSpaces, minor = false, width, height }) => {
 
 export default ({width, height}) => (
   <>
-    <Grid numSpaces={8} width={width} height={height}/>
-    <Grid numSpaces={8} minor width={width} height={height}/>
-    <BoundsMarkers width={width} height={height}/>
+    <Grid numSpaces={8} spacing={40} width={width} height={height}/>
+    <Grid numSpaces={8} minor spacing={40} width={width} height={height}/>
   </>
 );
