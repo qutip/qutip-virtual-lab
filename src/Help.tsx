e8814d2a86c138c2bd9f2e373aaa76e1dae25417
trentfridey <tfri.92@gmail.com>
2023-08-26 14:12:06 -0400

    update asset links and styling in help

--- src/Help.tsx
+++ src/Help.tsx
@@ -2,14 +2,14 @@ import './Help.css';
 
 import React, { useState } from 'react';
 
-import addInteraction from './assets/add_interaction.webm';
-import addQubit from './assets/add_qubit.webm';
-import addRemoveBath from './assets/add_remove_bath.webm';
-import changeInitialStates from './assets/change_initial_state.webm';
-import changeParameters from './assets/change_parameters.webm';
-import changeTimeSteps from './assets/change_time_steps.webm';
-import showResults from './assets/show_results.webm';
-import toggleLaser from './assets/toggle_laser.webm';
+import addInteraction from './assets/add_interaction.mp4';
+import addQubit from './assets/add_remove_qubits.mp4';
+import changeInitialStates from './assets/change_initial_state.mp4';
+import changeParameters from './assets/change_parameters.mp4';
+import changeTimeSteps from './assets/change_time_steps.mp4';
+import showResults from './assets/results.mp4';
+import addRemoveBath from './assets/toggle_bath.mp4';
+import toggleLaser from './assets/toggle_laser.mp4';
 
 export default function Help() {
     const [step, setStep] = useState(0)
@@ -30,7 +30,7 @@ export default function Help() {
         <div className="step-counter">{`(${step + 1}/${steps.length})`}</div>
         <div className="button-group">
             {step > 0 && <button onClick={prevStep}>Back</button>}
-            {step < steps.length - 1 ? <button onClick={nextStep}>Next</button> : <button>Finish</button>}
+            {step < steps.length - 1 ? <button onClick={nextStep}>Next</button> : <button onClick={() => setStep(0)}>Finish</button>}
         </div>
     </div>
 }
@@ -59,13 +59,13 @@ const LabStep = () => {
             </p>
             <div style={{ display: 'flex' }}>
                 <video controls width={300}>
-                    <source src={addQubit} type='video/webm' />
+                    <source src={addQubit} type='video/mp4' />
                 </video>
             </div>
             <p>You can add control lasers, interactions between qubits, and model heat baths from this menu</p>
             <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
                 <div style={{ flex: '0 1 auto' }}>
-                    <video controls height={300}>
+                    <video controls width={300}>
                         <source src={toggleLaser} />
                     </video>
                 </div>
@@ -92,19 +92,19 @@ const DetailStep = () => {
                 Here you can see the equations describing the Laboratory model.
                 You can tune the parameters that govern the lasers, interactions, and heat baths.
             </p>
-            <video controls>
+            <video controls width={300}>
                 <source src={changeParameters} />
             </video>
             <p>
                 You can change the initial state of each qubit to one of the eigenstates
             </p>
-            <video controls>
+            <video controls width={300}>
                 <source src={changeInitialStates} />
             </video>
             <p>
                 You can also tweak the simulation parameters for the time step and total time.
             </p>
-            <video controls>
+            <video controls width={300}>
                 <source src={changeTimeSteps}/>
             </video>
             
@@ -124,7 +124,7 @@ const ResultsStep = () => {
             <p>The evolution is plotted on the Bloch sphere</p>
             <p>You can also rotate the Bloch sphere</p>
             <p>Drag the slider at the bottom to view the system at a specific point in time</p>
-            <video controls>
+            <video controls width={300}>
                 <source src={showResults}/>
             </video>
         </>
@@ -137,6 +137,8 @@ const TabsStep = () => {
             <h1>Other Controls</h1>
             <p>The ⤢ button will toggle fullscreen mode</p>
             <p>The ⚛ button will allow you to select an example system. Try it!</p>
+            <h1>Still stuck?</h1>
+            <p>See the <a href="https://groups.google.com/g/qutip?pli=1">QuTiP users group</a></p>
         </>
     )
 }
